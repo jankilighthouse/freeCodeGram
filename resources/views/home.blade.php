bbe16e21140664d38ac9d7bc91b930f6ea11100b
Janaki Patel <janakipatel@Janakis-MacBook-Air.local>
2021-11-14 17:19:19 -0700

    wip..

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <img src="https://instagram.fyyc7-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fyyc7-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=wlLUSVLVc5MAX9Tq6no&edm=ABfd0MgBAAAA&ccb=7-4&oh=a4b9750ef49d0ad16439c49189d8288d&oe=61961727&_nc_sid=7bff83">
+        </div>
+        <div class="col-9">
+        </div>
+    </div>
+</div>
+@endsection
